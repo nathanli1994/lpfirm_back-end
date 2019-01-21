0409add1c81405ea9629dfcfa0e85cd7d5e8e59c
lihenan <33186874+nathanli1994@users.noreply.github.com>
2019-01-21 14:21:29 -0500

    rebuild total function
    replace total  func. now use review func to do statistic work

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -189,6 +189,138 @@ class User extends Base
         $this->assign('res1',$res1);
         $this->assign('this_year',$this_year);
         $this->assign('last_year',$last_year);
+
+
+
+        //拿到今年的业务
+        for($c=0;$c<count($business);$c++){
+            if(date('Y',$business[$c]) == $this_year){
+                $this_year_business[] = $business[$c];
+            }
+        }
+
+        //拿到去年的业务
+        for($c=0;$c<count($business);$c++){
+            if(date('Y',$business[$c]) == $last_year){
+                $last_year_business[] = $business[$c];
+            }
+        }
+
+
+        $timer_business = 0;
+        for($m=1;$m<=12;$m++){
+            //统计当前年份，每个月有多少客人
+            for ($a=0;$a<count($this_year_business);$a++){
+                if(date('m',$this_year_business[$a]) == $m){
+                    $timer_business += 1;
+                }
+            }
+            $res2[$m]['this_year_people_per_month'] = $timer_business;
+            $res2[$m]['month'] = $m;
+            $timer_business = 0;
+        }
+
+
+        $timer_business2 = 0;
+        for($m=1;$m<=12;$m++){
+            //统计去年，每个月有多少客人
+            for ($c=0;$c<count($last_year_business);$c++){
+                if(date('m',$last_year_business[$c]) == $m){
+                    $timer_business2 +=1;
+                }
+            }
+            $res2[$m]['last_year_people_per_month'] = $timer_business2;
+            $timer_business2 = 0;
+        }
+
+        $this->assign('res2',$res2);
+
+
+        //计算每天的钱
+        $today =strtotime(date('Y-m-d',time()));
+
+        $business_count = db('business')->whereTime('create_time',$today)->count();
+
+        if($business_count <= 1){
+            $business = db('business')->whereTime('create_time',$today)->find();
+            $result[] = $business;
+        }else{
+            $result = db('business')->whereTime('create_time',$today)->select()->toArray();
+        }
+
+        foreach ($result as $k=>$v){
+            if(empty($v['amount'])){
+                $v['amount'] = 0;
+            }
+            if(empty($v['post_fee'])){
+                $v['post_fee'] = 0;
+            }
+            if(empty($v['goverment_fee'])){
+                $v['goverment_fee'] = 0;
+            }
+            if(empty($v['explain_fee'])){
+                $v['explain_fee'] = 0;
+            }
+            if(empty($v['service_fee'])){
+                $v['service_fee'] = 0;
+            }
+            if(empty($v['subservice_name'])){
+                $v['subservice_name'] = "业务未找到";
+            }
+
+
+            if($v['subservice_name'] == '大学申请' || $v['subservice_name'] == 'college申请'){
+                $total_ideal_income[] = $v['amount'] + $v['post_fee'] + $v['goverment_fee'] + $v['explain_fee'];
+            }else{
+                $total_ideal_income[] = $v['service_fee'] + $v['post_fee'] + $v['goverment_fee'] + $v['explain_fee'];
+            }
+            $real_service_fee_income[] = $v['amount'];
+        }
+
+        $sum_ideal = array_sum($total_ideal_income);
+        $sum_real = array_sum($real_service_fee_income);
+        $short = $sum_ideal - $sum_real;
+        $this->assign('sum_ideal',$sum_ideal);
+        $this->assign('sum_real',$sum_real);
+        $this->assign('short',$short);
+
+
+        //计算月收入
+        //业务部分
+        $month = date('Y年n月',time());
+
+        $month_business_res = db('business')->whereTime('create_time','month')->select()->toArray();
+        foreach ($month_business_res as $k=>$v){
+            if(empty($v['amount'])){
+                $v['amount'] = 0;
+            }
+            if(empty($v['post_fee'])){
+                $v['post_fee'] = 0;
+            }
+            if(empty($v['goverment_fee'])){
+                $v['goverment_fee'] = 0;
+            }
+            if(empty($v['explain_fee'])){
+                $v['explain_fee'] = 0;
+            }
+            if(empty($v['service_fee'])){
+                $v['service_fee'] = 0;
+            }
+            if(empty($v['service_fee'])){
+                $v['subservice_name'] = $v['service_fee_change_reason'];
+            }
+
+
+            if($v['subservice_name'] == '大学申请' || $v['subservice_name'] == 'college申请'){
+                $month_income[] = $v['amount'] + $v['post_fee'] + $v['goverment_fee'] + $v['explain_fee'];
+            }else{
+                $month_income[] = $v['service_fee'] + $v['post_fee'] + $v['goverment_fee'] + $v['explain_fee'];
+            }
+        }
+        $sum_month_income = array_sum($month_income);
+        $this->assign('month',$month);
+        $this->assign('sum_month_income',$sum_month_income);
         return view();
     }
+
 }
